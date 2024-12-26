4761b860f3073515209ec05b866778dd7d37245b
Slavik H2 <spaike.moskov@mail.ru>
2024-12-26 23:32:04 +0300

    working with letters

--- wordle/Form1.h
+++ wordle/Form1.h
@@ -32,6 +32,7 @@ namespace Wordle {
             CreateMenu(); // Создание главного меню
             CreateGamePanel(); // Создание игровой панели
             SubscribeToKeyDownEvents(); // Подписка на события нажатия клавиш
+            SubscribeToTextChangedEvents(); // Подписка на события изменения текста
         }
 
     protected:
@@ -143,7 +144,7 @@ namespace Wordle {
             int x = 350, y = 180; // Начальные координаты для текстовых полей
             int width = 80, height = 70; // Ширина и высота текстовых полей
             int spacing = 1; // Промежуток между текстовыми полями
-            
+
 
             for (int i = 0; i < 6; i++) // 6 строк для попыток
             {
@@ -178,8 +179,8 @@ namespace Wordle {
             textBoxes[0]->Focus(); // Установка фокуса на первое текстовое поле
         }
 
-         // 13. Обработка нажатия на текстовое поле
-         void TextBox_Click(Object^ sender, EventArgs^ e) {
+        // 13. Обработка нажатия на текстовое поле
+        void TextBox_Click(Object^ sender, EventArgs^ e) {
             TextBox^ clickedTextBox = (TextBox^)sender;
             int index = Array::IndexOf(textBoxes, clickedTextBox);
             if (index >= attemptCount * 5 && index < (attemptCount + 1) * 5) {
@@ -187,143 +188,175 @@ namespace Wordle {
             }
         }
 
-         // 14. Подписка на события нажатия клавиш
-         void SubscribeToKeyDownEvents()
-         {
+        // 14. Подписка на события изменения текста
+        void SubscribeToTextChangedEvents()
+        {
             for (int i = 0; i < 30; i++)
-             {
-                 textBoxes[i]->KeyDown += gcnew KeyEventHandler(this, &Form1::TextBox_KeyDown);
-             }
-         }
-         // 15. Обработка нажатия клавиш в текстовом поле
-         void TextBox_KeyDown(Object^ sender, KeyEventArgs^ e) {
-             TextBox^ changedTextBox = (TextBox^)sender;
-             int index = Array::IndexOf(textBoxes, changedTextBox);
-
-             if (e->KeyCode == Keys::Enter) {
-                 CheckGuessedWord(); // Проверка введенного слова
-                 e->SuppressKeyPress = true;
-             }
-             else if (e->KeyCode == Keys::Back) {
-                 if (changedTextBox->Text->Length == 0 && index > 0) {
-                     textBoxes[index - 1]->Focus();
-                 }
-             }
-             else if (e->KeyCode == Keys::Space) {
-                 e->SuppressKeyPress = true;
-             }
-
-             // Запрещаем фокусировку на текстовых полях вне текущей строки
-             if (index < attemptCount * 5 || index >= (attemptCount + 1) * 5) {
-                 e->SuppressKeyPress = true;
-                 textBoxes[attemptCount * 5]->Focus();
-             }
-         }
-
-         // 16. Проверка угаданного слова
-         void CheckGuessedWord() {
-             if (attemptCount < 6 && IsCompleteWord()) {
-                 String^ guessedWord = "";
-                 for (int i = attemptCount * 5; i < (attemptCount + 1) * 5; i++) {
-                     guessedWord += textBoxes[i]->Text;
-                     textBoxes[i]->ReadOnly = true; // Блокировка текстовых полей после ввода слова
-                 }
-
-
-                 if (guessedWord->ToUpper() == correctWord->ToUpper()) {
-                     MessageBox::Show("Вы угадали слово!", "Поздравляем!", MessageBoxButtons::OK, MessageBoxIcon::Information);
-                     /*ClearGame();
-                     ShowMenu();*/
-                 }
-                 else {
-                     HighlightIncorrectLetters(guessedWord);
-                     attemptCount++;
-
-                     if (attemptCount < 6) {
-                         textBoxes[attemptCount * 5]->Focus();
-                     }
-                     else {
-                         MessageBox::Show("Вы исчерпали все попытки! Правильное слово: " + correctWord, "Игра окончена", MessageBoxButtons::OK, MessageBoxIcon::Information);
-                         /*ClearGame();
-                         ShowMenu();*/
-                     }
-                 }
-             }
-         }
-
-         // 17. Выделение неправильных букв
-         void HighlightIncorrectLetters(String^ guessedWord) {
-             array<wchar_t>^ guessedChars = guessedWord->ToUpper()->ToCharArray();
-             array<wchar_t>^ correctChars = correctWord->ToUpper()->ToCharArray();
-             array<bool>^ matchedCorrect = gcnew array<bool>(5); // Массив для отслеживания совпадений в правильном слове
-
-             for (int i = 0; i < 5; i++) {
-                 wchar_t guessedChar = guessedChars[i];
-                 wchar_t correctChar = correctChars[i];
-
-                 if (guessedChar == correctChar) {
-                     textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightGreen; // Правильная буква на правильном месте
-                     matchedCorrect[i] = true; // Отмечаем совпадение на этом месте
-                 }
-                 else {
-                     textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::White; // Остальные ячейки белого цвета
-                     matchedCorrect[i] = false; // Нет совпадения
-                 }
-             }
-
-             // Второй проход для отметки букв, которые есть в слове, но не на правильной позиции
-             for (int i = 0; i < 5; i++) {
-                 if (textBoxes[attemptCount * 5 + i]->BackColor == System::Drawing::Color::White) { // Только для не зеленых ячеек
-                     for (int j = 0; j < 5; j++) {
-                         if (!matchedCorrect[j] && guessedChars[i] == correctChars[j]) {
-                             textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightGoldenrodYellow; // Буква есть в слове, но не на месте
-                             matchedCorrect[j] = true; // Отмечаем, что эта буква использована
-                             break;
-                         }
-                     }
-                 }
-             }
-
-
-             // Третий проход для отметки букв, которые отсутствуют в слове
-             for (int i = 0; i < 5; i++) {
-                 if (textBoxes[attemptCount * 5 + i]->BackColor == System::Drawing::Color::White) {
-                     textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightCoral; // Буква отсутствует в слове
-                 }
-             }
-         }
-
-         // 18. Обработка изменения текста в текстовом поле
-         void TextBox_TextChanged(Object^ sender, EventArgs^ e) {
-             TextBox^ changedTextBox = (TextBox^)sender;
-
-             if (changedTextBox->Text->Length == 1) {
-                 int index = Array::IndexOf(textBoxes, changedTextBox);
-
-                 if (index < (attemptCount + 1) * 5 - 1) {
-                     textBoxes[index + 1]->Focus();
-                 }
-             }
-         }
-
-         // 19. Проверка, заполнено ли слово
-         bool IsCompleteWord()
-         {
-             for (int i = attemptCount * 5; i < (attemptCount + 1) * 5; i++)
-             {
-                 if (textBoxes[i]->Text->Length == 0)
-                 {
-                     return false; // Если хотя бы одно пустое, возвращаем false
-                 }
-             }
-             return true; // Если все заполнены, возвращаем true
-         }
-
-        private:
-            // Дополнительные настройки при загрузке формы
-            System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e)
             {
-                // Дополнительные настройки при загрузке формы, если необходимо
+                textBoxes[i]->TextChanged += gcnew System::EventHandler(this, &Form1::TextBox_TextChanged); // Подписка на событие изменения текста
             }
+        }
+
+        // 15. Подписка на события нажатия клавиш
+        void SubscribeToKeyDownEvents()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                textBoxes[i]->KeyDown += gcnew KeyEventHandler(this, &Form1::TextBox_KeyDown);
+            }
+        }
+        // 16. Обработка нажатия клавиш в текстовом поле
+        void TextBox_KeyDown(Object^ sender, KeyEventArgs^ e) {
+            TextBox^ changedTextBox = (TextBox^)sender;
+            int index = Array::IndexOf(textBoxes, changedTextBox);
+
+            if (e->KeyCode == Keys::Enter) {
+                CheckGuessedWord(); // Проверка введенного слова
+                e->SuppressKeyPress = true;
+            }
+            else if (e->KeyCode == Keys::Back) {
+                if (changedTextBox->Text->Length == 0 && index > 0) {
+                    textBoxes[index - 1]->Focus();
+                }
+            }
+            else if (e->KeyCode == Keys::Space) {
+                e->SuppressKeyPress = true;
+            }
+
+            // Запрещаем фокусировку на текстовых полях вне текущей строки
+            if (index < attemptCount * 5 || index >= (attemptCount + 1) * 5) {
+                e->SuppressKeyPress = true;
+                textBoxes[attemptCount * 5]->Focus();
+            }
+        }
+
+
+        // 17. Проверка угаданного слова
+        void CheckGuessedWord() {
+            if (attemptCount < 6 && IsCompleteWord()) {
+                String^ guessedWord = "";
+                for (int i = attemptCount * 5; i < (attemptCount + 1) * 5; i++) {
+                    guessedWord += textBoxes[i]->Text;
+                    textBoxes[i]->ReadOnly = true; // Блокировка текстовых полей после ввода слова
+                }
+
+                if (guessedWord->ToUpper() == correctWord->ToUpper()) {
+                    MessageBox::Show("Вы угадали слово!", "Поздравляем!", MessageBoxButtons::OK, MessageBoxIcon::Information);
+                    ClearGame();
+                    ShowMenu();
+                }
+                else {
+                    HighlightIncorrectLetters(guessedWord);
+                    attemptCount++;
+
+                    if (attemptCount < 6) {
+                        textBoxes[attemptCount * 5]->Focus();
+                    }
+                    else {
+                        MessageBox::Show("Вы исчерпали все попытки! Правильное слово: " + correctWord, "Игра окончена", MessageBoxButtons::OK, MessageBoxIcon::Information);
+                        ClearGame();
+                        ShowMenu();
+                    }
+                }
+            }
+        }
+
+        // 18. Выделение неправильных букв
+        void HighlightIncorrectLetters(String^ guessedWord) {
+            array<wchar_t>^ guessedChars = guessedWord->ToUpper()->ToCharArray();
+            array<wchar_t>^ correctChars = correctWord->ToUpper()->ToCharArray();
+            array<bool>^ matchedCorrect = gcnew array<bool>(5); // Массив для отслеживания совпадений в правильном слове
+
+            for (int i = 0; i < 5; i++) {
+                wchar_t guessedChar = guessedChars[i];
+                wchar_t correctChar = correctChars[i];
+
+                if (guessedChar == correctChar) {
+                    textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightGreen; // Правильная буква на правильном месте
+                    matchedCorrect[i] = true; // Отмечаем совпадение на этом месте
+                }
+                else {
+                    textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::White; // Остальные ячейки белого цвета
+                    matchedCorrect[i] = false; // Нет совпадения
+                }
+            }
+
+            // Второй проход для отметки букв, которые есть в слове, но не на правильной позиции
+            for (int i = 0; i < 5; i++) {
+                if (textBoxes[attemptCount * 5 + i]->BackColor == System::Drawing::Color::White) { // Только для не зеленых ячеек
+                    for (int j = 0; j < 5; j++) {
+                        if (!matchedCorrect[j] && guessedChars[i] == correctChars[j]) {
+                            textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightGoldenrodYellow; // Буква есть в слове, но не на месте
+                            matchedCorrect[j] = true; // Отмечаем, что эта буква использована
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            // Третий проход для отметки букв, которые отсутствуют в слове
+            for (int i = 0; i < 5; i++) {
+                if (textBoxes[attemptCount * 5 + i]->BackColor == System::Drawing::Color::White) {
+                    textBoxes[attemptCount * 5 + i]->BackColor = System::Drawing::Color::LightCoral; // Буква отсутствует в слове
+                }
+            }
+        }
+
+        // 19. Обработка изменения текста в текстовом поле
+        void TextBox_TextChanged(Object^ sender, EventArgs^ e) {
+            TextBox^ changedTextBox = (TextBox^)sender;
+
+            if (changedTextBox->Text->Length == 1) {
+                int index = Array::IndexOf(textBoxes, changedTextBox);
+
+                if (index < (attemptCount + 1) * 5 - 1) {
+                    textBoxes[index + 1]->Focus();
+                }
+            }
+        }
+
+
+        // 20. Проверка, заполнено ли слово
+        bool IsCompleteWord()
+        {
+            for (int i = attemptCount * 5; i < (attemptCount + 1) * 5; i++)
+            {
+                if (textBoxes[i]->Text->Length == 0)
+                {
+                    return false; // Если хотя бы одно пустое, возвращаем false
+                }
+            }
+            return true; // Если все заполнены, возвращаем true
+        }
+
+
+        // 21. Показать меню
+        void ShowMenu()
+        {
+            gamePanel->Visible = false; // Скрыть игровую панель
+            menuPanel->Visible = true; // Показать панель меню
+        }
+
+
+
+        // 22. Очистка игрового состояния
+        void ClearGame() {
+            for (int i = 0; i < 30; i++) {
+                textBoxes[i]->Text = "";
+                textBoxes[i]->BackColor = System::Drawing::Color::White;
+                textBoxes[i]->ReadOnly = false; // Разблокировка всех текстовых полей
+            }
+            attemptCount = 0;
+            SelectRandomWord();
+            textBoxes[0]->Focus();
+        }
+
+    private:
+        // 23. Дополнительные настройки при загрузке формы
+        System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e)
+        {
+            // Дополнительные настройки при загрузке формы, если необходимо
+        }
     };
 }
